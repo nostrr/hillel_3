61e2c260eb77706a4a5936043bd675dad090877b
Oleksii Lukavnov <lukavnev@gmail.com>
2023-04-05 00:08:42 +0300

    update Hometask 2

--- src/App.js
+++ src/App.js
@@ -11,28 +11,16 @@ class App extends React.Component {
     }
 
     updateData = () => {
-        let toggle = !this.state.toggle;
-        this.setState({name: 'Mykola', age: 30, toggle});
+        this.setState({name: 'Mykola', age: 30, toggle: !this.state.toggle});
     }
 
     render() {
-        let h1;
-        let text;
-        if (this.state.toggle) {
-            h1 = <h1>Абзац</h1>
-            text = 'Скрыть';
-        } else {
-            text = 'Показывать';
-        }
-
         return (
-            <>
                 <div>
-                    {h1}
+                    { this.state.toggle ? <h1>Абзац</h1> : ""}
                     <p>Name: {this.state.name}, age: {this.state.age}</p>
-                    <button onClick={this.updateData}>{text}</button>
+                    <button onClick={this.updateData}>{this.state.toggle ? 'Скрыть': 'Показывать' }</button>
                 </div>
-            </>
         )
     }
 }
