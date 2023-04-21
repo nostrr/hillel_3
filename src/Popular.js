ee3ef5b100a09664e6c8b9049b51fc9afed85ec2
Oleksii Lukavnov <lukavnev@gmail.com>
2023-04-22 00:08:02 +0300

    update 2 Hometask 5

--- src/Popular.js
+++ src/Popular.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import {fetchPopularRepos} from "./api";
 import {useSearchParams} from "react-router-dom";
 
-const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python", "C#"];
+
 
 const Popular = () => {
     const [selectedLanguage, setSelectedLanguage] = useState("All");
@@ -28,53 +28,73 @@ const Popular = () => {
     }, [activeTabFromSearchParams]);
 
     const onClickHandler = (language) => {
-        searchParams.set('tab', language);
-        window.history.replaceState({}, '', `${window.location.pathname}?${searchParams.toString()}`);
-        return showLoader ? null : setSelectedLanguage(language)
+        if (!showLoader) {
+            setSelectedLanguage(language)
+            searchParams.set('tab', language);
+            window.history.replaceState({}, '', `${window.location.pathname}?${searchParams.toString()}`);
+        }
     }
 
     return (
-        <>
-            {showLoader ? (<div>Loading....</div>)
-                :
-                (<div>
-                    <ul className="languages">
-                        {languages.map((language, index) => (
-                            <li key={index}
-                                style={{color: language === selectedLanguage ? '#d0021b' : '#000000'}}
-                                onClick={() => onClickHandler(language)}>
-                                {language}
-                            </li>))}
-                    </ul>
-                    <ul className='popular-list'>
-                        {repos.map((repo, index) => {
-                            return (
-                                <div key={repo.id}>
-                                    <li className='popular-item'>
-                                        <div className='popular-rank'>#{index + 1}</div>
-                                        <ul>
-                                            <li>
-                                                <img className='avatar' src={repo.owner.avatar_url} alt=''/>
-                                            </li>
-                                            <li>
-                                                <a href={repo.html_url} target='_blank'>{repo.name}</a>
-                                            </li>
-                                            <li>
-                                                @{repo.owner.login}
-                                            </li>
-                                            <li>
-                                                {repo.stargazers_count} starts
-                                            </li>
-                                        </ul>
-                                    </li>
-                                </div>
-                            )
-                        })}
-                    </ul>
-                </div>)}
-        </>
+        <div>
+            <Tab selectedLanguage={selectedLanguage} onClickHandler={onClickHandler}/>
+            {showLoader ? <Loader/>  : <RepList repos={repos}/>}
+        </div>
     )
+}
 
+function Repository(props){
+    return(
+        <li className='popular-item'>
+            <div className='popular-rank'>#{props.index + 1}</div>
+            <ul>
+                <li>
+                    <img className='avatar' src={props.repo.owner.avatar_url} alt=''/>
+                </li>
+                <li>
+                    <a href={props.repo.html_url} target='_blank'>{props.repo.name}</a>
+                </li>
+                <li>
+                    @{props.repo.owner.login}
+                </li>
+                <li>
+                    {props.repo.stargazers_count} stars
+                </li>
+            </ul>
+        </li>
+    )
+}
+
+function RepList(props){
+   return(
+       <ul className='popular-list'>
+        {props.repos.map((repo, index) => (
+           <Repository key={repo.id} index={index} repo={repo}/>
+        ))}
+    </ul>
+   )
+}
+
+const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python", "C#"];
+function Tab(props){
+    return(
+    <ul className="languages">
+        {languages.map((language, index) => (
+            <li key={index}
+                style={{color: language === props.selectedLanguage ? '#d0021b' : '#000000'}}
+                onClick={() => props.onClickHandler(language)}>
+                {language}
+            </li>))}
+    </ul>
+    )
+}
+
+function Loader() {
+    return (
+        <div className="loader-container">
+            <div className="loader"/>
+        </div>
+    );
 }
 
 export default Popular;
